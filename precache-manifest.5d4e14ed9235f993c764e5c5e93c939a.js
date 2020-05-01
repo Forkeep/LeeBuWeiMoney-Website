3df1493af2daa0f7f4983a3d43349794906eaf11
Forkeep <13126593533@163.com>
2020-05-01 11:47:39 +0800

    deploy

---
+++ precache-manifest.5d4e14ed9235f993c764e5c5e93c939a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51f56a6173e471a730f",
+    "url": "/LeeBuWeiMoney-Website/css/app.b2066007.css"
+  },
+  {
+    "revision": "8a1e958d1901bec873993454d007c580",
+    "url": "/LeeBuWeiMoney-Website/img/qrCode.png"
+  },
+  {
+    "revision": "55c16dcac7abc75698464ad86664096f",
+    "url": "/LeeBuWeiMoney-Website/index.html"
+  },
+  {
+    "revision": "f51f56a6173e471a730f",
+    "url": "/LeeBuWeiMoney-Website/js/app.8b0e6f76.js"
+  },
+  {
+    "revision": "084b3c10122632c53dc2",
+    "url": "/LeeBuWeiMoney-Website/js/chunk-vendors.9d7fb0f2.js"
+  },
+  {
+    "revision": "b0a93e17365a53ec64d4c7bf389c625b",
+    "url": "/LeeBuWeiMoney-Website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/LeeBuWeiMoney-Website/robots.txt"
+  }
+]);
